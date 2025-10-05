7d0721ed3695236d25586de890165983125a4feb
Dapa-yap <daffadwin10@gmail.com>
2025-10-05 19:13:11 +0700

    iFCetakKRS10

---
+++ MINGGU 7/iFCetakKRS10.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class iFCetakKRS10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("__Cetak KRS__");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean ukt = sc.nextBoolean();
+        if (ukt) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Pembayaran UKT Belum Terverifikasi");
+            System.out.println("Silahkan lakukan pembayaran UKT terlebih dahulu");
+        }
+    }
+}
+    

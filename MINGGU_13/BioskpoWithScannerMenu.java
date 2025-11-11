965375cc450f03334489c1c168181ea605d03ee1
Dapa-yap <daffadwin10@gmail.com>
2025-11-11 13:53:32 +0700

    modif34.2.2

--- MINGGU_13/BioskpoWithScannerMenu.java
+++ MINGGU_13/BioskpoWithScannerMenu.java
@@ -13,20 +13,28 @@ public class BioskpoWithScannerMenu {
             System.out.println("3. Exit");
             System.out.print("Pilih menu (1-3): ");
             int pilihan = sc.nextInt();
-            sc.nextLine(); 
-
-            if (pilihan == 1) {
-                System.out.print("Masukkan nama penonton: ");
-                String nama = sc.nextLine();
-                System.out.print("Masukkan baris: ");
-                int baris = sc.nextInt();
-                System.out.print("Masukkan kolom: ");
-                int kolom = sc.nextInt();
-                sc.nextLine();
-                penonton[baris - 1][kolom - 1] = nama;
-
-            } else if (pilihan == 2) {
-                System.out.println("\n=== DAFTAR PENONTON ===");
+            sc.nextLine();
+        
+        if (pilihan == 1) {
+            System.out.print("Masukkan nama penonton: ");
+            String nama = sc.nextLine();
+            System.out.print("Masukkan baris: ");
+            int baris = sc.nextInt();
+            System.out.print("Masukkan kolom: ");
+            int kolom = sc.nextInt();
+            sc.nextLine();
+            penonton[baris - 1][kolom - 1] = nama;
+            if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                System.out.println("Baris atau kolom tidak valid!");
+                continue;
+    
+            } 
+            if (penonton[baris - 1][kolom - 1] != null) {
+                System.out.println("Kursi sudah ditempati!");
+                continue;
+            }
+        } else if (pilihan == 2) {
+            System.out.println("\n=== DAFTAR PENONTON ===");
                 for (int i = 0; i < penonton.length; i++) {
                     System.out.print("Baris ke-" + (i + 1) + ": ");
                     for (int j = 0; j < penonton[i].length; j++) {
@@ -37,11 +45,9 @@ public class BioskpoWithScannerMenu {
                     }
                     System.out.println();
                 }
-
             } else if (pilihan == 3) {
                 System.out.println("Terima kasih! Program selesai.");
                 break;
-
             } else {
                 System.out.println("Pilihan tidak valid, coba lagi!");
             }

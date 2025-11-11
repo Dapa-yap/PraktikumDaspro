3e36ab277139078b3e708d24faf2245bc8bb7dc5
Dapa-yap <daffadwin10@gmail.com>
2025-11-11 13:16:57 +0700

    2.4

--- MINGGU_13/SIAKAD10.java
+++ MINGGU_13/SIAKAD10.java
@@ -12,7 +12,17 @@ public class SIAKAD10 {
             nilai[i][j] = sc.nextInt();
             total += nilai[i][j];
         }
-        System.out.println("Nilai rata rata" + total/3);
+        System.out.println("Nilai rata rata " + total/3);
+    }
+        System.out.println("\n==============================");
+        System.out.println("Rata rata Nilai setiap mata kuliah: ");
+        for (int j = 0; j < 3; j++) {
+            double totalPermatkul = 0;
+            for (int k = 0; k < 4; k++) {
+                totalPermatkul += nilai[k][j];
+            }
+            System.out.println("Nilai rata rata mata kuliah ke- " + (j+1) + ": " + totalPermatkul/4);
+        }
      }   
     }
-}
+

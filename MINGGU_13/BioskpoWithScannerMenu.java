9b3d44a52f3c113b4f597a11c3780a9ba7156c43
Dapa-yap <daffadwin10@gmail.com>
2025-11-11 13:45:58 +0700

    modif2,2.2

---
+++ MINGGU_13/BioskpoWithScannerMenu.java
@@ -0,0 +1,67 @@
+package MINGGU_13;
+import java.util.Scanner;
+
+public class BioskpoWithScannerMenu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.println("\n=== MENU BIOSKOP MINI ===");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1-3): ");
+            int pilihan = sc.nextInt();
+            sc.nextLine(); 
+
+            if (pilihan == 1) {
+                System.out.print("Masukkan nama penonton: ");
+                String nama = sc.nextLine();
+                System.out.print("Masukkan baris (1-4): ");
+                int baris = sc.nextInt();
+                System.out.print("Masukkan kolom (1-2): ");
+                int kolom = sc.nextInt();
+                sc.nextLine();
+
+                if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                    System.out.println("Baris atau kolom tidak valid!");
+                    continue;
+                }
+
+                if (penonton[baris - 1][kolom - 1] != null) {
+                    System.out.println("Kursi sudah ditempati!");
+                } else {
+                    penonton[baris - 1][kolom - 1] = nama;
+                    System.out.println("Data penonton berhasil dimasukkan!");
+                }
+
+            } else if (pilihan == 2) {
+                System.out.println("\n=== DAFTAR PENONTON ===");
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.print("Baris ke-" + (i + 1) + ": ");
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(penonton[i][j]);
+                        }
+                        if (j < penonton[i].length - 1) {
+                            System.out.print(", ");
+                        }
+                    }
+                    System.out.println();
+                }
+
+            } else if (pilihan == 3) {
+                System.out.println("Terima kasih! Program selesai.");
+                break;
+
+            } else {
+                System.out.println("Pilihan tidak valid, coba lagi!");
+            }
+        }
+
+        sc.close();
+    }
+}

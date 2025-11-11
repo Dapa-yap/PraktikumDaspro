56f1214c827418781842280ca90cf707a17bc3a8
Dapa-yap <daffadwin10@gmail.com>
2025-11-11 12:41:24 +0700

    soal1,2.3

---
+++ MINGGU_13/numbers10.java
@@ -0,0 +1,14 @@
+package MINGGU_13;
+
+public class numbers10 {
+    public static void main(String[] args) {
+        int [][] myNumbers = new int [3][];
+        myNumbers[0] = new int [5];
+        myNumbers[1] = new int [3];
+        myNumbers[2] = new int [1];
+
+        for (int i = 0; i < myNumbers.Length; i++) {
+           System.out.println(Arrays.toString(myNumbers[i]));
+            }
+        }
+    }
